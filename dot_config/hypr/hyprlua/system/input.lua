ef9e42ef0b68b7412658d3c9d9d65be486e0a94f
dirichy <byl315121270@gmail.com>
2026-03-09 16:44:58 +0800

    fix: use wofi to input and no need to chenge focus. usr paste instead of wtype.

--- dot_config/hypr/hyprlua/system/input.lua
+++ dot_config/hypr/hyprlua/system/input.lua
@@ -2,7 +2,22 @@ local uv = hypr.uv
 local M = {}
 local handle
 local function wtype(str)
-	hypr.cmd.wtype(str)
+	if handle then
+		handle:kill()
+		handle:close()
+		handle = nil
+	end
+	local stdin = uv.new_pipe()
+	handle = uv.spawn("wl-copy", {
+		stdio = { stdin, nil, nil },
+	}, function(code, signal)
+		uv.close(handle)
+		handle = nil
+		local timer = uv.new_timer()
+		hypr.keybind.sendkey("v", hypr.keybind.modifier.CTRL)
+	end)
+	stdin:write(str)
+	stdin:close()
 end
 function M.prompt(title, prompt, callback)
 	if handle then
@@ -13,15 +28,18 @@ function M.prompt(title, prompt, callback)
 	title = title or "Hyprlua Input"
 	prompt = prompt or "Please Input:"
 	local stdout = uv.new_pipe(false)
-	handle = uv.spawn("zenity", {
+	handle = uv.spawn("wofi", {
 		args = {
-			"--entry",
-			"--title='" .. title .. "'",
-			"--text='" .. prompt .. "'",
+			"--dmenu",
+			"--width",
+			"400",
+			"--height",
+			"50",
 		},
 		stdio = { nil, stdout, nil },
 	}, function(code, signal)
 		uv.close(handle)
+		handle = nil
 	end)
 	local output = ""
 	uv.read_start(stdout, function(err, data)
@@ -29,12 +47,12 @@ function M.prompt(title, prompt, callback)
 			output = output .. data
 		else
 			output = output:sub(1, -2)
-			if callback then
-				callback(output)
-			else
-				wtype(output)
-			end
 			stdout:close()
+			if callback then
+				return callback(output)
+			else
+				return wtype(output)
+			end
 		end
 	end)
 end
