0fe96c5e15555585373117f09e7ebc2b99bbc01d
evawyy <1216687256@qq.com>
2026-04-08 09:42:49 +0800

    feat: nvim: config vgit.nvim

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1489,6 +1489,7 @@ return {
 				{ "<leader>b", group = "Buffer" },
 				{ "<leader>t", group = "LaTeX", icon = { cat = "filetype", name = "tex" } },
 				{ "<leader>f", group = "Find", icon = { icon = "", color = "blue" } },
+				{ "<leader>g", group = "Git" },
 				{ "<leader>q", group = "Session" },
 				{ "g", group = "Goto" },
 				{ "<leader>u", group = "Toggle" },
