b03e2b65b1748bb19542d997528404aba2f083c0
dirichy <byl315121270@gmail.com>
2026-05-06 00:42:53 +0800

    feat: rime: update tiger code lua script

--- dot_local/share/fcitx5/rime/lua/clear_on_missing_code.lua
+++ dot_local/share/fcitx5/rime/lua/clear_on_missing_code.lua
@@ -1,8 +1,34 @@
 local commit_history = ""
+local last_space_time = 0
 local clear_on_missing_code = function(key, env)
 	local input = env.engine.context.input
 	local commit = env.engine.context:get_commit_text()
+	if key.keycode == string.byte(" ") then
+		local ctime = rime_api.get_time_ms()
+		if ctime < last_space_time + 100 then
+			return 1
+		end
+		last_space_time = ctime
+		if #input == 0 then
+			return 2
+		end
+		commit = commit:gsub("[a-z]", "")
+		if #commit == 0 then
+			env.engine.context:clear()
+			return 1
+		end
+		commit_history = commit:sub(utf8.offset(commit, -1))
+		env.engine:commit_text(commit)
+		env.engine.context:clear()
+		return 1
+	end
+	if #input == 0 then
+		return 2
+	end
 	if key.keycode == string.byte(";") and #input % 4 == 1 and string.sub(input, -1, -1) == "z" then
+		if #input == 0 then
+			return 2
+		end
 		if #input == 1 then
 			env.engine:commit_text(commit_history)
 		else
@@ -12,13 +38,16 @@ local clear_on_missing_code = function(key, env)
 		env.engine.context:clear()
 		return 1
 	end
-	if key.keycode == string.byte(" ") then
-		-- env.engine:commit_text(commit .. "::")
-		commit = commit:gsub("[a-z]*$", "")
+	if key.keycode == string.byte(",") or key.keycode == string.byte(".") then
+		commit = commit:gsub("[a-z]", "")
+		if #commit == 0 then
+			env.engine.context:clear()
+			return 1
+		end
 		commit_history = commit:sub(utf8.offset(commit, -1))
-		env.engine:commit_text(string.gsub(commit, "%a*$", ""))
+		env.engine:commit_text(commit)
 		env.engine.context:clear()
-		return 1
+		return 2
 	end
 	-- env.engine:commit_text(tostring(key.keycode))
 	-- env.engine.context:clear()
