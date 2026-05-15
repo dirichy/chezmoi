59ea8d1e851270ddbc1c6b8a698d55806787208b
dirichy <byl315121270@gmail.com>
2026-05-15 22:27:45 +0800

    feat: yabai: auto fullscreen mpv

---
+++ dot_config/yabai/executable_fullscreen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+if [ -z "$YABAI_WINDOW_ID" ]; then
+  YABAI_WINDOW_ID=$1
+fi
+
+YABAI_CURRENT_SPACE=$(
+  /opt/homebrew/bin/yabai -m query --windows --window "$YABAI_WINDOW_ID" \
+    | jq -r '.space'
+  )
+
+  YABAI_NEW_SPACE_INDEX=$(
+    /opt/homebrew/bin/yabai -m query --spaces \
+      | jq 'length + 1'
+    )
+    /opt/homebrew/bin/yabai -m window --toggle native-fullscreen
+    /opt/homebrew/bin/yabai -m query --spaces --space | jq -e '.index > 9' &&
+      /opt/homebrew/bin/yabai -m space \
+      "$YABAI_NEW_SPACE_INDEX" \
+      --label "fullscreen$YABAI_CURRENT_SPACE"
