6cf71a1aa7a8b3eb57f955951f4adc5895d483ab
dirichy <byl315121270@gmail.com>
2026-09-05 20:56:08 +0800

    fix: nvim: fix lua_ls for nvim config

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -213,10 +213,6 @@ return {
 							diagnostics = {
 								globals = { "vim" },
 							},
-							workspace = {
-								library = vim.api.nvim_get_runtime_file("", true),
-								checkThirdParty = false,
-							},
 							telemetry = {
 								enable = false,
 							},
