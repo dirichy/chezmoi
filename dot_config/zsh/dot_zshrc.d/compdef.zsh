cd14076a9af4997bccd8559d79034b0737a8bb73
dirichy <byl315121270@gmail.com>
2026-09-06 16:32:03 +0800

    feat: zsh: define complation per program

--- dot_config/zsh/dot_zshrc.d/compdef.zsh
+++ dot_config/zsh/dot_zshrc.d/compdef.zsh
@@ -1,15 +1,18 @@
 comp_ext() {
-  local cmd=$1
-  local -a extensions=("${@:2}")
-  local pattern="*.${(j:|:)extensions}"
-  (( ${#extensions} > 1 )) && pattern="*.(${(j:|:)extensions})"
+    local cmd="$1"
+    shift
+    local pattern
+    if [[ -z $2 ]]; then
+        pattern="*.${(j:|:)@}"
+    else
+        pattern="*.(${(j:|:)@})"
+    fi
 
-  compdef _files "$cmd"
-  zstyle ":completion:*:*:${cmd}:*:*" \
-    file-patterns "$pattern:${cmd}\ files" '*(-/):directories'
+    local func="_comp_ext_${cmd}"
+    functions[$func]="
+    _arguments '*:file:_files -g \"$pattern\"'
+  "
+    compdef $func $cmd
 }
-
 comp_ext sioyek pdf
-comp_ext zathura pdf
-comp_ext mpv mp4 mkv webm avi mov m4v flv mpg mpeg png jpg jpeg gif webp
-comp_ext imv png jpg jpeg gif webp bmp avif tiff
+comp_ext mpv mp4 png jpg
